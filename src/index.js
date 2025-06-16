e0e7ee28b2562cd3b113bbb2faaf6b90a4ba2289
Victor Seleimend <sseleimend@gmail.com>
2025-06-16 12:15:03 -0300

    Add static file serving middleware to Express app

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const app = express();
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 const corsOptions = {
   origin: process.env.ORIGIN,
