e088ae44e38dea7847d3fed7806458ce28780c81
Victor Seleimend <sseleimend@gmail.com>
2025-06-16 13:08:20 -0300

    Refactor app configuration by consolidating middleware setup and export methods

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -10,12 +11,35 @@ if (env.IS_LOCAL) {
   dotenv.config();
 }
 
-function configureApp(app) {
+const app = express();
+
+function configure() {
+  app.use(express.json({ limit: "16kb" }));
+  app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+  app.use(express.static("public"));
+
   const corsOptions = {
     origin: process.env.ORIGIN,
     credentials: true,
   };
   app.use(cors(corsOptions));
+
+  return app;
 }
 
-export default configureApp;
+function listen() {
+  const PORT = parseInt(process.env.PORT) || 3001;
+
+  app.listen(PORT, () => {
+    console.log(
+      `App listening on port ${PORT} in the${env.IS_LOCAL ? " local" : ""} ${env.NODE_ENV} environment`,
+    );
+  });
+
+  return app;
+}
+
+export default {
+  configure,
+  listen,
+};
--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
-import express from "express";
-
 import connectToDatabase from "./db/index.js";
-import configureApp from "./config/config.js";
-import { env } from "./utils/constants.js";
-
-const app = express();
-
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
-
-configureApp(app);
+import app from "./config/config.js";
 
 bootstrap();
 
 async function bootstrap() {
   try {
     await connectToDatabase();
-
-    const PORT = parseInt(process.env.PORT) || 3001;
-    app.listen(PORT, () => {
-      console.log(
-        `App listening on port ${PORT} in the${env.IS_LOCAL ? " local" : ""} ${env.NODE_ENV} environment`,
-      );
-    });
+    app.configure();
+    app.listen();
   } catch (error) {
     console.error(error);
     process.exit(1);
