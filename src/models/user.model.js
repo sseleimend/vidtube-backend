71fceaa04dbbc085f7cb2e1f851ff2b0fe8414a5
Victor Seleimend <sseleimend@gmail.com>
2025-06-17 10:46:24 -0300

    Add User model with schema for user details including username, email, fullname, and password

---
+++ src/models/user.model.js
@@ -0,0 +1,61 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+      maxLength: [100, "Username cannot be more than 100 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+      validate: {
+        validator: function (email) {
+          return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+        },
+        message: () => `Please enter a valid email address`,
+      },
+    },
+    fullname: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+      maxLength: [100, "Fullname cannot be more than 100 characters"],
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const User = mongoose.model("User", userSchema);
