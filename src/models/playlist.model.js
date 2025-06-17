ce2db13f355e62424aa186a0fe47f06341d189ed
Victor Seleimend <sseleimend@gmail.com>
2025-06-17 10:46:08 -0300

    Add Playlist model with schema for playlists including name, description, videos, and owner

---
+++ src/models/playlist.model.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
