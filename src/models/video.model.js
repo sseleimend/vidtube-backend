a345038400919df941ee1d1318db9109721025b4
Victor Seleimend <sseleimend@gmail.com>
2025-06-17 10:46:29 -0300

    Add Video model with schema for video details including file, thumbnail, title, description, duration, views, publication status, and owner

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: [true, "Video file is required"],
+    },
+    thumbnail: {
+      type: String,
+      required: [true, "Thumbnail is required"],
+    },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Video = mongoose.model("Video", videoSchema);
