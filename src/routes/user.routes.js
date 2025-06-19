8b580ba2d00f3ec3bfcde8fe071b39e83bb27bbe
Victor Seleimend <sseleimend@gmail.com>
2025-06-19 11:16:32 -0300

    Add logout route with JWT verification to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -17,5 +18,7 @@ router.route("/register").post(
   ]),
   registerUser,
 );
+
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
